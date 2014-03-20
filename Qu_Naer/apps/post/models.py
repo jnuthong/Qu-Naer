6c99ab4801c2ccdb32fdf4ba63b7f5b65cbc1712
Aviatorjeremy <aviatorjeremy@gmail.com>
2014-03-20 15:16:48 +0800

    post CRUD

--- Qu_Naer/apps/post/models.py
+++ Qu_Naer/apps/post/models.py
@@ -2,8 +2,12 @@
 __author__ = 'gong'
 __create_time__ = '10/03/2014'
 
+
+import datetime
+#import zlib
 from django.db import models
 from django.forms.models import model_to_dict
+from django.utils.timezone import utc
 
 
 class Post(models.Model):
@@ -11,7 +15,7 @@ class Post(models.Model):
     user_id = models.BigIntegerField(blank=True, null=True)
     book_id = models.BigIntegerField(blank=True, null=True)
     place_id = models.BigIntegerField(blank=True, null=True)
-    post_content = models.BinaryField(blank=True, null=True)
+    post_content = models.CharField(blank=True, null=True)    # without compress
     post_image = models.CharField(max_length=32, blank=True)
     post_source = models.SmallIntegerField(blank=True, null=True)
     post_type = models.SmallIntegerField(blank=True, null=True)
@@ -33,7 +37,27 @@ class Post(models.Model):
                                              'post_status', 'post_audit', 'post_image_meta'))
         return fields
 
+    def __str__(self):
+        return '%s, %s, %s, %s' % (self.id, self.user_id, self.book_id, self.place_id)
 
+    def create_one_post(self, *args, **kwargs):
+        if not self.create_time:
+            self.create_time = datetime.datetime.utcnow().replace(tzinfo=utc)
+        self.update_time = datetime.datetime.utcnow().replace(tzinfo=utc)
+        super(Post, self).save(*args, **kwargs)
+        return self
+
+    def get_one_post(self, post_id):
+        return self.objects.get(id=post_id)
+
+    def get_posts_by_user(self, user_id):
+        return self.objects.filter(user_id=user_id)
+
+    def update_post(self, post_id, **kwargs):
+        self.objects.filter(id=post_id).update(**kwargs)
+
+    def delete_post(self, post_id):
+        self.objects.filter(id=post_id).update(post_status=1)
 
 
 class SharedPost(models.Model):

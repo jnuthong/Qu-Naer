5700b4508143c9c87eb73245cbc3e055b51e7921
Aviatorjeremy <aviatorjeremy@gmail.com>
2015-09-24 21:08:34 +0800

    comment models

--- Qu_Naer/apps/comment/models.py
+++ Qu_Naer/apps/comment/models.py
@@ -3,7 +3,7 @@
 __author__ = 'gong'
 __create_time__ = '22/09/2015'
 
-from django.db import models
+from django.db import models, manager
 from django.forms.models import model_to_dict
 from django.utils.timezone import utc
 import datetime
@@ -15,13 +15,15 @@ class Comment(models.Model):
     """
     Comment have foreign key for some specify post
     """
-    user_id = models.BigIntegerField(blank=True, null=False, help_text='user id of commend')
+    comment_id = models.AutoField(primary_key=True)
+    game_id = models.BigIntegerField(blank=True, null=False)
+    user_id = models.BigIntegerField(blank=True, null=False)
+    mark_id = models.BigIntegerField(blank=False, null=False)
     comment_content = models.CharField(max_length=Comment_Length, blank=True, null=True)
-    post_id = models.BigIntegerField(blank=True, null=False)
-    post_user_id = models.BigIntegerField(blank=True, null=False)
-    discuss_comment_id = models.BigIntegerField(blank=True, null=True)
-    discuss_user_id = models.BigIntegerField(blank=True, null=True)
-    comment_type = models.SmallIntegerField(blank=True, null=True, help_text='is this comment is a re-comment')
+    game_picture = models.CharField(max_length=128, blank=True, null=True)
+    reply_comment_id = models.BigIntegerField(blank=True, null=True)
+    reply_user_id = models.BigIntegerField(blank=True, null=True)
+    reply_flag = models.SmallIntegerField(blank=True, null=True, help_text='is this comment is a re-comment')
     create_time = models.DateTimeField(blank=True, null=True)
     update_time = models.DateTimeField(blank=True, null=True)
     comment_source = models.SmallIntegerField(blank=True, default=0,
@@ -30,13 +32,13 @@ class Comment(models.Model):
 
     class Meta:
         db_table = 'rdb_comment'
-        ordering = ['user_id', '-update_time']
-        index_together = [
-            ["post_id", "comment_status"],
-            ["user_id", "update_time", "comment_status"],
-            ["post_user_id", "update_time", "comment_status"],
-            ["discuss_user_id", "update_time", "comment_status"],
-        ]
+        #ordering = ['user_id', '-update_time']
+        #index_together = [
+        #    ["post_id", "comment_status"],
+        #    ["user_id", "update_time", "comment_status"],
+        #    ["post_user_id", "update_time", "comment_status"],
+        #    ["discuss_user_id", "update_time", "comment_status"],
+        #]
 
     def save(self, *args, **kwargs):
         """
@@ -45,7 +47,7 @@ class Comment(models.Model):
         if not self.create_time:
             self.create_time = datetime.datetime.utcnow().replace(tzinfo=utc)
         self.update_time = datetime.datetime.utcnow().replace(tzinfo=utc)
-        super(MComment, self).save(*args, **kwargs)
+        super(Comment, self).save(*args, **kwargs)
         return self
 
     def update_by_comment_id(self, *arg, **kwargs):
@@ -74,6 +76,13 @@ class Comment(models.Model):
         Delete a comment by comment id, here use hard delete, which mean real delete, please care with this operation
         """
         try:
-            self.delete()
+            self.comment_status = 1
+            self.save()
         except Exception as e:
             return dict(msg="Error in delete comment:comment_id: , error msg: %s" % str(e))
+
+
+class Mark(models.Model):
+    """
+    Mark model for games
+    """
